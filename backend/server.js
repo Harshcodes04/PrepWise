1e7305e803ed37b2d51f685872c5ccbd71210181
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-06-06 00:04:45 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config()  
+const app=require("./src/app")
+const connectDB=require("./src/config/database")
+
+
+app.listen(5432,async()=>{
+    try{
+        await connectDB();
+        console.log("server started on port http://localhost:5432")
+    }
+    catch(error){
+        console.log(error)
+        process.exit(1)
+    }
+
+})
