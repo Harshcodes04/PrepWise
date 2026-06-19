ad69b7228af17f1861f4ec3c24ad4fcfcdbe410c
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-06-19 21:52:31 +0530

    added interview routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,18 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-const cors=require("cors")
-const cookieParser = require("cookie-parser")
-const authRouter = require("./routes/auth.routes")
-const clientUrl = process.env.CLIENT_URL || "http://localhost:5173"
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
+const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
 
-app.use(express.json())
-app.use(cors({credentials:true,origin: clientUrl}))
+app.use(express.json());
+app.use(cors({ credentials: true, origin: clientUrl }));
 app.use(cookieParser());
 
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-
-module.exports = app
+module.exports = app;
---
+++ backend/src/routes/interview.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const authMiddleware = require("../middleware/auth.middleware");
+const interviewController = require("../controllers/interview.controller");
+const interviewRouter = express.Router();
+const upload = require("../middlewares/file.middleware");
+
+interviewRouter.post(
+  "/",
+  authMiddleware.authUser,
+  upload.single("resume"),
+  interviewController.generateInterviewReportController,
+);
+
+module.exports = interviewRouter;
