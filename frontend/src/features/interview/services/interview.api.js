a46374c2dcba373ed5bdf9986d19c89a9829fc42
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-06-27 00:45:12 +0530

    Add interview API client helpers

---
+++ frontend/src/features/interview/services/interview.api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
+
+export const generateInterviewReport = async ({
+  jobDescription,
+  selfDescription,
+  resumeFile,
+}) => {
+  const formData = new FormData();
+  formData.append("jobDescription", jobDescription);
+  formData.append("selfDescription", selfDescription);
+  formData.append("resume", resumeFile);
+
+  const response = await api.post("/api/interview/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response;
+};
+//frontend to backend file will be sent using formData and the backend will receive it using multer middleware. The backend will then process the file and generate the interview report based on the provided job description and self description.
+
+export const getInterviewReportById = async (interviewId) => {
+  const response = await api.get(`/api/interview/report/${interviewId}`);
+  return response.data.interviewReport;
+};
+
+export const getAllInterviewReports = async () => {
+  const response = await api.get("/api/interview/");
+  return response.data;
+};
